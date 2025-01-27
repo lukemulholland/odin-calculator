b078f87ff387b0163839d79d32e2945658fb9217
Luke Mulholland <lukemulholland@gmail.com>
2025-01-28 06:43:18 +1100

    Add basic arithmetic functions and operate function

---
+++ script.js
@@ -0,0 +1,49 @@
+function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    return a + b;
+}
+
+function subtract(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    return a - b;
+}
+
+function multiply(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    return a * b;
+}
+
+function divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new Error('Both arguments must be numbers');
+    }
+    if (b === 0) {
+        throw new Error('Division by zero is not allowed');
+    }
+    return a / b;
+}
+
+let firstNumber = 0 
+let secondNumber = 0
+let operator = String
+
+function operate(firstNumber, secondNumber, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+        default:
+            throw new Error('Invalid operator');
+    }
+}
